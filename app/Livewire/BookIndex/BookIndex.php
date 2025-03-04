ff7d7d2f9f90c34e098bc774290fd17853ba6f2f
EslamKamel89 <elsamkamelforex@gmail.com>
2025-03-04 15:12:47 +0200

    implement pagination for book index and enhance no books found message

--- app/Livewire/BookIndex/BookIndex.php
+++ app/Livewire/BookIndex/BookIndex.php
@@ -6,16 +6,18 @@ use App\Livewire\Forms\BookForm;
 use App\Models\Book;
 use Livewire\Attributes\On;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class BookIndex extends Component {
-	use \App\Traits\Pr;
+	use \App\Traits\Pr, WithPagination;
 	protected $listeners = [ 
 		'book-index.update' => '$refresh'
 	];
 
 	public function render() {
+
 		return view( 'livewire.book-index.book-index', [ 
-			'books' => auth()->user()->books()->latest()->get(),
+			'books' => auth()->user()->books()->latest()->paginate( 5 ),
 		] );
 	}
 	public function deleteBook( $id ) {
--- resources/views/livewire/book-index/book-index.blade.php
+++ resources/views/livewire/book-index/book-index.blade.php
@@ -1,8 +1,18 @@
 <div>
     <livewire:book-index.create-book />
-    <div class="flex flex-col gap-2">
-        @foreach ( $books as $book )
-			<livewire:book-index.book-item :book="$book" wire:key="{{ $book->id }}" />
-		@endforeach
-    </div>
+    <div id="books"></div>
+    @if ( $books->count() > 0 )
+		<div class="flex flex-col gap-2">
+			@foreach ( $books as $book )
+				<livewire:book-index.book-item :book="$book" wire:key="{{ $book->id }}" />
+			@endforeach
+		</div>
+		<div class="mt-4">
+			{{ $books->links( data: [ 'scrollTo' => false ] ) }}
+		</div>
+	@else
+		<div class="flex justify-center items-center h-64">
+			<p class="text-gray-500">No books found</p>
+		</div>
+	@endif
 </div>
